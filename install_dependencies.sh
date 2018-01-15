09f8dff82466b6bffe73bab2d0e636746901550e
Peter Kowalsky <peter@comhire>
2018-01-15 11:19:18 +0100

    added libelf-dev

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,4 +3,4 @@
 echo "Fetching updates..."
 sudo apt-get update
 echo "Installing dependencies for kernel compilation..."
-sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc kernel-package
+sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc kernel-package libelf-dev
